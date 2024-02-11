975a98c2ff7f6c016ee78d602a00c5959343d190
Zeinab Mahmoud <zeinab.mahmoud12997@gmail.com>
2024-02-11 02:18:23 +0200

    radio tab ui finished

--- lib/home/tabs/radio_tab.dart
+++ lib/home/tabs/radio_tab.dart
@@ -6,7 +6,41 @@ class RadioTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("assets/images/radio_image.png"),
+          SizedBox(
+            height: 12,
+          ),
+          Text("إذاعة القرآن الكريم"),
+          SizedBox(
+            height: 12,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(onPressed: onPreviousPressed, icon: Icon(Icons.skip_previous, color: Color(0xffB7935F), size: 45)),
+              IconButton(onPressed: onPlayPressed, icon: Icon(Icons.play_arrow, color: Color(0xffB7935F), size: 64,)),
+              IconButton(onPressed: onNextPressed, icon: Icon(Icons.skip_next, color: Color(0xffB7935F), size: 45))
+            ],
+          )
+        ],
+      ),
     );
+  }
+
+
+  void onPlayPressed(){
+
+  }
+
+  void onNextPressed(){
+
+  }
+
+  void onPreviousPressed(){
+
   }
 }
